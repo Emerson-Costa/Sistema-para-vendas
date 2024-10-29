92e43d50431c257afef6a54f270573d02dc44ddc
Emerson Costa <emersonjscosta@gmail.com>
2024-10-29 16:21:58 -0300

    Conexão com Banco de dados H2 e criação do modelo categoria.

--- src/main/java/com/emerson/vendas/domain/Categoria.java
+++ src/main/java/com/emerson/vendas/domain/Categoria.java
@@ -1,15 +1,25 @@
 package com.emerson.vendas.domain;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.io.Serializable;
 
+@Entity
 public class Categoria  implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
-    public Categoria() { }
+    public Categoria() {
+
+    }
 
     public Categoria(Integer id, String nome) {
         this.id = id;
