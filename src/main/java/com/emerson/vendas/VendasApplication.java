aab74ee3d57d33b0b3780a0f636e6c900aa8ded0
Emerson Costa <emersonjscosta@gmail.com>
2024-11-01 12:35:57 -0300

    Criado na main objetos para instanciação de dados do banco.

--- src/main/java/com/emerson/vendas/VendasApplication.java
+++ src/main/java/com/emerson/vendas/VendasApplication.java
@@ -1,14 +1,36 @@
 package com.emerson.vendas;
 
+import com.emerson.vendas.domain.Categoria;
+import com.emerson.vendas.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(VendasApplication.class, args);
 	}
 
+	/*
+	* Essa ação é executada quando a palicação inicia.
+	* A implementação é baseada em instancias que são armazenadas no banco de dados temporário h2.
+	* A implementaçao do código utiliza a Interface Repository para ter acesso ao banco de dados.
+	* */
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
