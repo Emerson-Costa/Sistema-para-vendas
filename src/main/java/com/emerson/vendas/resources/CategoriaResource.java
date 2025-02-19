22e72d8758f59e0704b4242986882becef8492c5
Emerson Costa <emersonjscosta@gmail.com>
2025-02-19 15:53:53 -0300

    feat: Listando todas as categorias com DTO.

--- src/main/java/com/emerson/vendas/resources/CategoriaResource.java
+++ src/main/java/com/emerson/vendas/resources/CategoriaResource.java
@@ -1,17 +1,15 @@
 package com.emerson.vendas.resources;
 
 import com.emerson.vendas.domain.Categoria;
+import com.emerson.vendas.dto.CategoriaDTO;
 import com.emerson.vendas.services.CategoriaService;
-import jakarta.servlet.Servlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -29,7 +27,8 @@ public class CategoriaResource {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Void> insert(@RequestBody  Categoria obj) {
+    public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest().path("/{id}")
@@ -39,16 +38,26 @@ public class CategoriaResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+    public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> delete(@PathVariable Integer id){
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+        List<Categoria> obj = service.findAll();
+        List<CategoriaDTO> listDTO = obj.stream().map(CategoriaDTO::new).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
--- src/main/java/com/emerson/vendas/services/CategoriaService.java
+++ src/main/java/com/emerson/vendas/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,25 +17,28 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public  Categoria find(Integer id) {
+    public Categoria find(Integer id) {
+
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(
                 () -> new ObjectNotFoundException("Objeto não encontrado! Id:" + id + ", Tipo: "
-                + Categoria.class.getName())
-        );
+                        + Categoria.class.getName()));
     }
 
-    public Categoria insert(Categoria obj){
+    public Categoria insert(Categoria obj) {
+
         obj.setId(null);
         return repo.save(obj);
     }
 
-    public Categoria update(Categoria obj){
+    public Categoria update(Categoria obj) {
+
         find(obj.getId());
         return repo.save(obj);
     }
 
-    public void delete(Integer id){
+    public void delete(Integer id) {
+
         find(id);
 
         try {
@@ -44,6 +48,11 @@ public class CategoriaService {
 
             throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
         }
+    }
+
+    public List<Categoria> findAll() {
+
+        return repo.findAll();
     }
 
 }
